b1b140402c8540fbc847fe086ef6202c62396ea2
Nao Minami <south37777@gmail.com>
2015-12-10 18:25:26 +0900

    Delete unused code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,2 @@
 class User < ActiveRecord::Base
-  has_many :friend_relations
-  has_many :friends, through: :friend_relations
-
-  CREATE_FRIEND_COUNT_STORED_PROCEDURE_SQL = <<-SQL
-CREATE OR REPLACE FUNCTION friend_count(integer) RETURNS integer AS
-$$
-DECLARE
-  ret integer;
-BEGIN
-  SELECT COUNT(*) INTO ret FROM friend_relations WHERE user_id = $1;
-  RETURN ret;
-END;
-$$
-LANGUAGE 'PLPGSQL'
-  SQL
-
-  DROP_FRIEND_COUNT_STORED_PROCEDURE_SQL = <<-SQL
-DROP FUNCTION friend_count(integer);
-  SQL
 end
