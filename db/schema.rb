3b756f74f6875c4a5a25b2acb0bf6a6b58190a55
Nao Minami <south37777@gmail.com>
2015-12-10 19:05:38 +0900

    Add event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210073021) do
+ActiveRecord::Schema.define(version: 20151210095702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20151210073021) do
 
   create_table "companies", force: :cascade do |t|
     t.text     "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.json     "payload"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
