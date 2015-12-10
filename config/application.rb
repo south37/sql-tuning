e5169959dc6bb2060e957bfa01606fd224078eab
Nao Minami <south37777@gmail.com>
2015-12-10 15:35:36 +0900

    Add generator config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module SpRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
   end
 end
