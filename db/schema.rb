4151d42d483f483d9d9c02c6a94f7dd4e620a4b6
Nao Minami <south37777@gmail.com>
2015-12-10 15:41:28 +0900

    Add company

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210063920) do
+ActiveRecord::Schema.define(version: 20151210064024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.text     "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.integer  "id_without_index"
